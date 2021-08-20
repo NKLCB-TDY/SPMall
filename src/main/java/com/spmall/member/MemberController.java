908637337be8b737f70b3f571cb62727c8c7f06a
jaehong2 <dtg3431@gmail.com>
2021-08-21 01:04:32 +0900

    Mod Member(Cont,Serv)

--- src/main/java/com/spmall/member/MemberController.java
+++ src/main/java/com/spmall/member/MemberController.java
@@ -1,12 +1,19 @@
 package com.spmall.member;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
@@ -19,11 +26,75 @@ public class MemberController {
 	MemberService memberService;
 	
 	
-	//회원가입 창
-	@RequestMapping(value ="/memberSignUp.do", method = RequestMethod.GET)
-	public String memberSignUp() {
-		return "memberSignUp";
+	//회원가입 페이지로 이동
+	@RequestMapping(value ="/membersignupform.do", method = RequestMethod.GET)
+	public String membersignupform() {
+		return "membersignupform";
 	}
+	
+	//id 겹치는지 확인
+	@RequestMapping(value ="/idcheck.do", method = RequestMethod.POST)
+	public void idoverlap(@RequestParam("member_id") String member_id) {
+		
+		String a = memberService.idcheck(member_id);
+	}
+	
+	//회원가입 로직 
+	@RequestMapping(value="/membersignup.do", method = RequestMethod.POST)
+	public ModelAndView membersignup(@ModelAttribute("membervo") MemberVO vo) {
+		memberService.signupMember(vo);
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
+	
+	//로그인 폼 이동
+	@RequestMapping("/memberloginform.do")
+	public String memberloginform() {
+		return "memberloginform";
+	}
+	
+	//로그인 처리
+	@RequestMapping(value = "/memberlogin.do", method = RequestMethod.POST)
+	public void login(@RequestParam("member_id") String member_id,
+					  @RequestParam("member_pwd") String member_pwd) throws IOException{
+		
+	}
+	
+	//회원 정보 업데이트 폼 이동
+	@RequestMapping(value = "/memberupdateform.do", method = RequestMethod.POST)
+	public String memberupdateform() {
+		return "memberupdateform";
+	}
+	
+	//회원 업데이트 처리
+	@RequestMapping(value = "/memberupdate.do", method = RequestMethod.POST)
+	public ModelAndView memberupdate(@ModelAttribute("membervo") MemberVO vo) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/redirect:/main/main.do");
+		
+		return mav;
+	}
+	
+	//로그아웃 처리
+	@RequestMapping(value = "/memberlogout.do", method = RequestMethod.GET)
+	public ModelAndView memberlogout(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/main/main.do");
+		HttpSession session = request.getSession();
+		session.removeAttribute("");
+		return mav; 
+	}
+	
+	//회원 정보 확인 페이지 이동
+	@RequestMapping(value="/memberInfo.do")
+	public String memberInfo() {
+		//세션 확인 작업 추가해야됨
+		return "memberInfo";
+	}
+	
+	//비회원 추가 한다면
+	/* public */
+	
 	
 	
 }
